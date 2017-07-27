ed87a45114296ec70a53d7106c3bbca366a2b5c4
Eric Blanchard <Eric.x2@gmail.com>
2017-07-27 09:49:04 -0400

    Added two player functionality

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-var canvas = document.getElementById("board")
-var width = canvas.width
-var height = canvas.height
-var ctx = canvas.getContext("2d")
-ctx.fillStyle = "green"
-var snake;
-var counter = 0;
+var canvas = document.getElementById("board");
+var players = 2;
+var btn1Player = document.getElementById("1PlayerBTN");
+var btn2Player = document.getElementById("2PlayerBTN");
+var btnStart = document.getElementById("Start");
+var txtMessage = document.getElementById("Message");
+var width = canvas.width;
+var height = canvas.height;
+var ctx = canvas.getContext("2d");
+var snakes = [];
 var counterMax = 5;
+
 var state = {
   x: (width / 2),
   y: (height / 2),
@@ -13,11 +17,13 @@ var state = {
     left: false,
     right: false,
     up: false,
-    down: false
-  },
-  size: 1,
-  isPowerup: false
-}
+    down: false,
+    left2: false,
+    right2: false,
+    up2: false,
+    down2: false
+  }
+};
 var timer;
 var keyMap = {
   68: 'right',
@@ -26,18 +32,35 @@ var keyMap = {
   83: 'down',
   27: 'escape',
   32: 'space',
-  13: 'enter'
-}
+  13: 'enter',
+  38: 'up2',
+  39: 'right2',
+  40: 'down2',
+  37: 'left2'
+};
+var startSpots = [
+	{x: width / 4, y: height / 4 * 3},
+	{x: width / 4 * 3, y: height / 4 * 3}
+];
 
-function Snake(){
+function Snake(playerNum){
 	this.width = 2;
+	this.counter = 0;
 	this.height = 2;
 	this.speed = 2;
-	this.body = [{x: width / 2, y: height / 2}];
+	this.player = playerNum;
+	var copy = JSON.parse(JSON.stringify(startSpots));
+	this.body = [copy[playerNum]]
 	this.direction = 0;
 	this.isAboutToGrow = false;
 	this.Draw = function() {
 		var length = this.body.length;
+		if (this.player == 0){
+			ctx.fillStyle = "green"
+		}
+		else if(this.player == 1){
+			ctx.fillStyle = "blue"
+		}
 		for (var i = 0; i < length; i++){
 			ctx.fillRect(this.body[i].x - this.width / 2, this.body[i].y - this.height / 2, this.width, this.height)
 		}
@@ -53,19 +76,19 @@ function Snake(){
 		}
 		switch (direction){
 			case 0:
-				this.body[0].y -= snake.speed;
+				this.body[0].y -= this.speed;
 				this.direction = direction;
 				break;
 			case 1:
-				this.body[0].x += snake.speed;
+				this.body[0].x += this.speed;
 				this.direction = direction;
 				break;
 			case 2:
-				this.body[0].y += snake.speed;
+				this.body[0].y += this.speed;
 				this.direction = direction;
 				break;
 			case 3:
-				this.body[0].x -= snake.speed;
+				this.body[0].x -= this.speed;
 				this.direction = direction;
 				break;
 			default:
@@ -88,62 +111,97 @@ function keyup(event) {
   state.pressedKeys[key] = false
 }
 
+function SetPlayers(numPlayers){
+	players = numPlayers;
+	txtMessage.innerHTML = "Game has been set to " + (numPlayers) + " player(s).";
+	ClearScreen();
+};
 
 function Initialize(){
-	snake = new Snake();
+	snakes = [];
+	for (var i = 0; i < players; i++){
+		snakes[i] = new Snake(i);
+	}
 }
 
-function Update(){
-	counter++;
-	var moved = false;
-	if (counter > counterMax){
-		counter = 0;
-		snake.isAboutToGrow = true;;
-	}
-	if (state.pressedKeys.left) {
-    	snake.Move(3);
-    	moved = true;
-  	}
-  	if (state.pressedKeys.right) {
-    	snake.Move(1);
-    	moved = true;
-  	}
-	if (state.pressedKeys.up) {
-	    snake.Move(0);
-	    moved = true;
-	  }
-	if (state.pressedKeys.down) {
-	    snake.Move(2);
-	    moved = true;
-	  }
-	//Move the snake even if a new direction isn't entered
-	if (moved == false){
-		snake.Move(snake.direction);
-	}
-	//Check for edge collisions
-	if (snake.body[0].x < 0){
-		Initialize();
-	}
-	else if (snake.body[0].x > width){
-		Initialize();
-	}
-	else if (snake.body[0].y < 0){
-		Initialize();
-	}
-	else if (snake.body[0].y > height){
-		Initialize();
-	}
-	//Check for snake collisions
-	for (var i = 1; i < snake.body.length; i++){
-		if (snake.body[0].x == snake.body[i].x && snake.body[0].y == snake.body[i].y){
-			Initialize();
+function Update(){	
+	for (var i = 0; i < snakes.length; i++){
+		snakes[i].counter++;
+		var moved = [false, false];
+		if (snakes[i].counter > counterMax){
+			snakes[i].counter = 0;
+			snakes[i].isAboutToGrow = true;;
+		}
+		if (state.pressedKeys.left) {
+	    	snakes[0].Move(3);
+	    	moved[0] = true;
+	  	}
+	  	if (state.pressedKeys.right) {
+	    	snakes[0].Move(1);
+	    	moved[0] = true;
+	  	}
+		if (state.pressedKeys.up) {
+		    snakes[0].Move(0);
+		    moved[0] = true;
+		}
+		if (state.pressedKeys.down) {
+		    snakes[0].Move(2);
+		    moved[0] = true;
+		}
+		if (state.pressedKeys.left2) {
+	    	snakes[1].Move(3);
+	    	moved[1] = true;
+	  	}
+	  	if (state.pressedKeys.right2) {
+	    	snakes[1].Move(1);
+	    	moved[1] = true;
+	  	}
+		if (state.pressedKeys.up2) {
+		    snakes[1].Move(0);
+		    moved[1] = true;
+		}
+		if (state.pressedKeys.down2) {
+		    snakes[1].Move(2);
+		    moved[1] = true;
+		}
+		//Move the snake even if a new direction isn't entered
+		if (moved[i] == false){
+			snakes[i].Move(snakes[i].direction);
+		}
+		//Check for edge collisions
+		if (snakes[i].body[0].x < 0){
+			EndGame(i);
+		}
+		else if (snakes[i].body[0].x > width){
+			EndGame(i);
+		}
+		else if (snakes[i].body[0].y < 0){
+			EndGame(i);
+		}
+		else if (snakes[i].body[0].y > height){
+			EndGame(i);
+		}
+		//Check for snake collisions
+		for (var j = 1; j < snakes[0].body.length; j++){
+			if (snakes[i].body[0].x == snakes[0].body[j].x && snakes[i].body[0].y == snakes[0].body[j].y){
+				EndGame(i);
+			}
+		}
+		if (players == 2){
+			for (var j = 1; j < snakes[1].body.length; j++){
+				if (snakes[i].body[0].x == snakes[1].body[j].x && snakes[i].body[0].y == snakes[1].body[j].y){
+					EndGame(i);
+				}
+			}
 		}
 	}
 };
 
 function Draw(){
 	ctx.clearRect(0, 0, width, height)
-	snake.Draw();		
+	for (var i = 0; i < snakes.length; i++){
+		snakes[i].Draw();	
+	}		
 };
 
 function Loop(){
@@ -151,7 +209,22 @@ function Loop(){
 	Draw();
 };
 
+function ClearScreen(){
+	ctx.clearRect(0, 0, width, height)
+}
+
+function EndGame(playerDied){
+	txtMessage.innerHTML = ("Player " + (playerDied+1) + " has lost.");
+	clearInterval(timer);
+}
+
+function StartGame(){
+	Initialize();
+	timer = setInterval(Loop, 50);
+	txtMessage.innerHTML = ("Begin!");
+}
+btn1Player.addEventListener("click", function() {SetPlayers(1)});
+btn2Player.addEventListener("click", function() {SetPlayers(2)});
+btnStart.addEventListener("click", function() {StartGame()});
 window.addEventListener("keydown", keydown, false);
 window.addEventListener("keyup", keyup, false);
-snake = new Snake();
-timer = setInterval(Loop, 50);
